f8013452fbae1c1ac53585613f7bb59e60cf6ea8
HyungjuJang <dacapolife87@gmail.com>
2020-06-11 21:10:52 +0900

    jUnit - Tag어노테이션 추가 (fast / slow) - Tag 어노테이션 - 파라미터 @ParameterizedTest / @ValueSource - OS별 테스트 @EnabledOnOs - 조건에 따른 테스트 assumingTest

--- src/test/java/org/hjjang/junitexample/StudyTest.java
+++ src/test/java/org/hjjang/junitexample/StudyTest.java
@@ -3,15 +3,66 @@ package org.hjjang.junitexample;
 //import org.junit.jupiter.api.*;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.lang.reflect.Executable;
 import java.time.Duration;
+import java.util.Map;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
+
+    @DisplayName("스터디 만들기 반복 \uD83D\uDE31")
+    @RepeatedTest(value = 10,name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void create_study_repeatTest(RepetitionInfo repetitionInfo){
+        System.out.println("RepeatTest : "+repetitionInfo.getCurrentRepetition() + "/"+
+                repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("파라미터 메시지")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고","았네요."})
+    void parameterizedTest(String message){
+        System.out.println(message);
+    }
+    @Test
+    @DisplayName("스터디 만들기 @madeByFast \uD83D\uDE31")
+    @FastTest
+    void create_new_study_madeTagByFast(){
+        Study actual = new Study(100);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+        System.out.println("create_new_study_madeTagByFast");
+    }
+
+
+    @Test
+    @DisplayName("스터디 만들기 Fast \uD83D\uDE31")
+    @Tag("fast")
+    void create_new_study_fast(){
+        Study actual = new Study(100);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+        System.out.println("create_new_study_fast");
+    }
+
+    @Test
+    @DisplayName("스터디 만들기 Slow \uD83D\uDE31")
+    @Tag("slow")
+    void create_new_study_slow(){
+        Study actual = new Study(100);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+        System.out.println("create_new_study_slow");
+    }
+
 
     @Test
     @DisplayName("크레이트1")
@@ -40,6 +91,56 @@ class StudyTest {
     }
 
     @Test
+    @DisplayName("OS환경 \uD83D\uDE31")
+    @EnabledOnOs(OS.WINDOWS)
+    void os_test_byWin(){
+        System.out.println("Windows");
+    }
+    @Test
+    @DisplayName("OS환경 \uD83D\uDE31")
+    @EnabledOnOs(OS.LINUX)
+    void os_test_byLinux(){
+        System.out.println("linux");
+    }
+
+    @Test
+    @DisplayName("OS환경_윈도우 테스트안함 \uD83D\uDE31")
+    @DisabledOnOs(OS.WINDOWS)
+    void os_test_byWin_Disabled(){
+        System.out.println("Disabled Window Test");
+    }
+
+    @Test
+    @DisplayName("OS환경_멀티 \uD83D\uDE31")
+    @EnabledOnOs({OS.WINDOWS, OS.LINUX})
+    void os_test_byMulti(){
+        System.out.println("multi");
+    }
+
+    @Test
+    @DisplayName("환경변수 \uD83D\uDE31")
+    void env_test(){
+        System.out.println("env_test");
+        String test_env = System.getenv("TEST_ENV");
+
+        System.out.println(test_env);
+//        assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+
+        assumingThat("LOCAL".equalsIgnoreCase(test_env),() -> {
+            System.out.println("Local");
+            Study actual = new Study(10);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        });
+
+        assumingThat("hjjang".equalsIgnoreCase(test_env),() -> {
+            System.out.println("hJJANG");
+            Study actual = new Study(10);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        });
+
+    }
+
+    @Test
     @Disabled
     void craete2(){
         System.out.println("craete2");
@@ -61,7 +162,7 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기2 \uD83D\uDE31")
     void create1_new_study2(){
-        assertTimeout(Duration.ofSeconds(100), () -> {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
             new Study(10);
             Thread.sleep(300);
         });
